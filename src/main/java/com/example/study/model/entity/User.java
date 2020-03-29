2e743c4eb6a4ff4a1a344979ac8b1372fc52178a
shkim4738 <shkim4738@gmail.com>
2020-03-29 20:45:58 +0900

    Entity

---
+++ src/main/java/com/example/study/model/entity/User.java
@@ -0,0 +1,34 @@
+package com.example.study.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@Entity // == table
+//@Table(name = "user") // 클래스 이름이 테이블 이름과 동일하면 자동으로 mapping 된다.
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //@Column(name = "account") // 필드의 이름과 테이블 컬럼의 이름이 동일하면 자동으로 mapping 된다.
+    private String account;
+
+    private String email;
+
+    private String phoneNumber;
+
+    private LocalDateTime createdAt;
+
+    private String createdBy;
+
+    private LocalDateTime updatedAt;
+
+    private String updatedBy;
+
+}
