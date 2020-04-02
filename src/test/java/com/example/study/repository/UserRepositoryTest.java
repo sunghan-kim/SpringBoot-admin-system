35464152bbd16ca7b97febc681bdb18486621c06
shkim4738 <shkim4738@gmail.com>
2020-04-02 22:17:27 +0900

    User Repository Test

--- src/test/java/com/example/study/repository/UserRepositoryTest.java
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -19,13 +19,36 @@ public class UserRepositoryTest extends StudyApplicationTests {
 
     @Test
     public void create() {
+        String account = "Test01";
+        String password = "Test01";
+        String status = "REGISTERED";
+        String email = "Test01@gmail.com";
+        String phoneNumber = "010-1111-2222";
+        LocalDateTime registeredAt = LocalDateTime.now();
+        LocalDateTime createdAt = LocalDateTime.now();
+        String createdBy = "AdminServer";
 
+        User user = new User();
+        user.setAccount(account);
+        user.setPassword(password);
+        user.setStatus(status);
+        user.setEmail(email);
+        user.setPhoneNumber(phoneNumber);
+        user.setRegisteredAt(registeredAt);
+        user.setCreatedAt(createdAt);
+        user.setCreatedBy(createdBy);
+
+        User newUser = userRepository.save(user);
+
+        Assert.assertNotNull(newUser);
     }
 
     @Test
     @Transactional
     public void read() {
+        User user = userRepository.findFirstByPhoneNumberOrderByIdDesc("010-1111-2222");
 
+        Assert.assertNotNull(user);
     }
 
     @Test
