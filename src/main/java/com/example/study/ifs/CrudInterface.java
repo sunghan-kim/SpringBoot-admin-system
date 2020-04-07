8ea7ef720c4e2503a2bf8ad387d0ddc976f81722
shkim4738 <shkim4738@gmail.com>
2020-04-07 22:05:43 +0900

    CRUD 인터페이스에 Request, Response 제네릭 지정

--- src/main/java/com/example/study/ifs/CrudInterface.java
+++ src/main/java/com/example/study/ifs/CrudInterface.java
@@ -2,13 +2,13 @@ package com.example.study.ifs;
 
 import com.example.study.model.network.Header;
 
-public interface CrudInterface {
+public interface CrudInterface<Req, Res> {
 
-    Header create(); // TODO: Request Object 추가
+    Header<Res> create(Req request); // TODO: Request Object 추가
 
-    Header read(Long id);
+    Header<Res> read(Long id);
 
-    Header update();
+    Header<Res> update(Req request);
 
     Header delete(Long id);
 
