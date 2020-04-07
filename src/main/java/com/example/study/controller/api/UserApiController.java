2a46c7bb6fe5b28ce1130e25adac205397aa222f
shkim4738 <shkim4738@gmail.com>
2020-04-07 22:36:31 +0900

    User(사용자) API - CREATE

--- src/main/java/com/example/study/controller/api/UserApiController.java
+++ src/main/java/com/example/study/controller/api/UserApiController.java
@@ -4,16 +4,24 @@ import com.example.study.ifs.CrudInterface;
 import com.example.study.model.network.Header;
 import com.example.study.model.network.request.UserApiRequest;
 import com.example.study.model.network.response.UserApiResponse;
+import com.example.study.service.UserApiLogicService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
+@Slf4j
 public class UserApiController implements CrudInterface<UserApiRequest, UserApiResponse> {
+
+    @Autowired
+    private UserApiLogicService userApiLogicService;
 
     @Override
     @PostMapping("") // /api/user
-    public Header<UserApiResponse> create(@RequestBody Header<UserApiRequest> userApiRequest) {
-        return null;
+    public Header<UserApiResponse> create(@RequestBody Header<UserApiRequest> request) {
+        log.info("{}", request); // -> request.toString()
+        return userApiLogicService.create(request);
     }
 
     @Override
---
+++ src/main/java/com/example/study/service/UserApiLogicService.java
@@ -0,0 +1,78 @@
+package com.example.study.service;
+
+import com.example.study.ifs.CrudInterface;
+import com.example.study.model.entity.User;
+import com.example.study.model.network.Header;
+import com.example.study.model.network.request.UserApiRequest;
+import com.example.study.model.network.response.UserApiResponse;
+import com.example.study.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class UserApiLogicService implements CrudInterface<UserApiRequest, UserApiResponse> {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // 1. get request data
+    // 2. create user
+    // 3. 생성된 데이터를 기준으로 UserApiResponse return
+
+    @Override
+    public Header<UserApiResponse> create(Header<UserApiRequest> request) {
+
+        // 1. get request data
+        UserApiRequest  userApiRequest = request.getData();
+
+        // 2. create user
+        User user = User.builder()
+                .account(userApiRequest.getAccount())
+                .password(userApiRequest.getPassword())
+                .status("REGISTERED")
+                .phoneNumber(userApiRequest.getPhoneNumber())
+                .email(userApiRequest.getEmail())
+                .registeredAt(LocalDateTime.now())
+                .build();
+
+        User newUser = userRepository.save(user);
+
+        // 3. 생성된 데이터를 기준으로 UserApiResponse return
+        return response(newUser);
+    }
+
+    @Override
+    public Header<UserApiResponse> read(Long id) {
+        return null;
+    }
+
+    @Override
+    public Header<UserApiResponse> update(Header<UserApiRequest> request) {
+        return null;
+    }
+
+    @Override
+    public Header delete(Long id) {
+        return null;
+    }
+
+    private Header<UserApiResponse> response(User user) {
+        // user -> userApiResponse
+        UserApiResponse userApiResponse = UserApiResponse.builder()
+                .id(user.getId())
+                .account(user.getAccount())
+                .password(user.getPassword()) // TODO : 암호화, 길이
+                .status(user.getStatus())
+                .email(user.getEmail())
+                .phoneNumber(user.getPhoneNumber())
+                .registeredAt(user.getRegisteredAt())
+                .unregisteredAt(user.getUnregisteredAt())
+                .build();
+
+        // Header + data return
+        return Header.OK(userApiResponse);
+    }
+
+}
