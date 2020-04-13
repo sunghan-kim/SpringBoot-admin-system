3cab2919a247e853dadc06ca44043bf534a741ab
shkim4738 <shkim4738@gmail.com>
2020-04-13 21:32:17 +0900

    OrderGroup(주문내역) API - READ

--- src/main/java/com/example/study/service/OrderGroupApiLogicService.java
+++ src/main/java/com/example/study/service/OrderGroupApiLogicService.java
@@ -44,7 +44,9 @@ public class OrderGroupApiLogicService implements CrudInterface<OrderGroupApiReq
 
     @Override
     public Header<OrderGroupApiResponse> read(Long id) {
-        return null;
+        return orderGroupRepository.findById(id)
+                .map(this::response) // == .map(orderGroup -> response(orderGroup)) == .map(orderGroup -> {return response(orderGroup)})
+                .orElseGet(() -> Header.ERROR("데이터 없음"));
     }
 
     @Override
