96e8e0a88b4ee8dfd0cecbd2b91ae044193b01f2
shkim4738 <shkim4738@gmail.com>
2020-04-14 21:15:27 +0900

    Controller 추상화

--- src/main/java/com/example/study/controller/api/UserApiController.java
+++ src/main/java/com/example/study/controller/api/UserApiController.java
@@ -1,47 +1,27 @@
 package com.example.study.controller.api;
 
-import com.example.study.ifs.CrudInterface;
-import com.example.study.model.network.Header;
+import com.example.study.controller.CrudController;
 import com.example.study.model.network.request.UserApiRequest;
 import com.example.study.model.network.response.UserApiResponse;
 import com.example.study.service.UserApiLogicService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
 
 @RestController
 @RequestMapping("/api/user")
 @Slf4j
-public class UserApiController implements CrudInterface<UserApiRequest, UserApiResponse> {
+public class UserApiController extends CrudController<UserApiRequest, UserApiResponse> {
 
     @Autowired
     private UserApiLogicService userApiLogicService;
 
-    @Override
-    @PostMapping("") // /api/user
-    public Header<UserApiResponse> create(@RequestBody Header<UserApiRequest> request) {
-        log.info("{}", request); // -> request.toString()
-        return userApiLogicService.create(request);
-    }
-
-    @Override
-    @GetMapping("{id}") // /api/user/{id}
-    public Header<UserApiResponse> read(@PathVariable(name = "id") Long id) {
-        log.info("read id : {}", id);
-        return userApiLogicService.read(id);
-    }
-
-    @Override
-    @PutMapping("") // /api/user
-    public Header<UserApiResponse> update(@RequestBody Header<UserApiRequest> request) {
-        return userApiLogicService.update(request);
-    }
-
-    @Override
-    @DeleteMapping("{id}") // /api/user/{id}
-    public Header delete(@PathVariable(name = "id") Long id) {
-        log.info("delete : {}", id);
-        return userApiLogicService.delete(id);
+    @PostConstruct
+    public void init() {
+        this.baseService = userApiLogicService;
     }
 
 }
